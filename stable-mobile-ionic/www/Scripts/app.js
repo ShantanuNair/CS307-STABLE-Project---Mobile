da304917437de8b7d43feede5ba64a4ecc796fe6
ShantanuNair <nairs@purdue.edu>
2016-04-21 13:07:13 -0400

    Robustness for /

--- stable-mobile-ionic/www/Scripts/app.js
+++ stable-mobile-ionic/www/Scripts/app.js
@@ -340,6 +340,7 @@ app.controller('vaccineFormController', [ '$scope', '$firebaseArray',"Auth", '$h
             "price": $scope.vaccinePrice,
         };
 
+        if (typeof vaccine.name != "string" || typeof vaccine.name === null || vaccine.name.indexOf("/") > -1 ) {
             alert("Please provide a valid vaccine name");
         }
         else if (typeof vaccine.seller != "string" || typeof vaccine.seller === null) {
@@ -386,6 +387,7 @@ app.controller('trainingFormController', [ '$scope', '$firebaseArray', '$http', 
             "equipment": $scope.trainingEquipment,
         };
 
+        if (typeof regimen.name != "string" || typeof regimen.name=== null || regimen.name.indexOf("/") > -1 z) {
             alert("Please provide a valid regimen name");
         }
         else if (typeof regimen.exercise != "string" || typeof regimen.exercise  === null) {
